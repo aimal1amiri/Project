76393b1d254a71d19971abdb0b7a05c5557c7208
emir2269 <ali724345@gmail.com>
2025-02-04 00:07:30 +0300

    bug is fixed. and designed the setting page.

--- Front-end/src/globalState/themeGlobalState.js
+++ Front-end/src/globalState/themeGlobalState.js
@@ -1,7 +1,9 @@
 import {create} from 'zustand'
 
 export const themeGlobalState = create((set)=>({
-    themeColor: localStorage.getItem("chat-theme") || "dark" ,
+    themeColor: localStorage.getItem("chat-theme") || "dark",
+    
+
     setThemeColor: (themeColor)=> {
         localStorage.setItem("chat-theme",themeColor)
         set({ themeColor });}
