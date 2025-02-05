e6d13846304eb5de7e7932ef2b7a757a607261f1
emir2269 <ali724345@gmail.com>
2025-02-05 11:37:58 +0300

    designing the -chatPage- and implementing the functionalities for getting the data for chat

---
+++ Front-end/src/globalState/chatGlobalState.js
@@ -0,0 +1,46 @@
+import {create} from 'zustand'
+import toast from 'react-hot-toast';
+import { axiosURL } from '../lib/Backnd-info-fetching';
+
+
+export const chatGlobalState = create((set) =>({
+    messages:[],
+    users:[],
+    selectedUser:null,
+    usersLoading:false,
+    messagesLoading:false,
+
+
+    getUser: async () =>{
+        set({usersLoading:true})
+
+        try {
+            const response = axiosURL.get("/message/users")
+
+            set({users:response.data});
+
+
+        } catch (error) {
+            toast.error(error.response.data.message);
+            
+        }finally{
+            set({usersLoading:false});
+        }
+    },
+
+    getMessages:async ()=>{
+        set({messagesLoading:true})
+
+        try {
+            const response = axiosURL.get(`/message/${userId}`)
+            set({messages:response.data})
+            
+        } catch (error) {
+            toast.error(error.response.data.message)
+            
+        }finally{
+            set({messagesLoading:false})
+        }
+    }
+
+}))
